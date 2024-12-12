21138e93d51aa6c3f1dcc60c05194eef188468c2
amang9446 <dev.aman7571@gmail.com>
2024-12-12 23:11:39 +0530

    Initial Commit

---
+++ components/ProductListItem.tsx
@@ -0,0 +1,9 @@
+import { Text } from "react-native";
+
+export default function ProductListItem({ product }) {
+    return (
+        <Text style={{ fontSize: 30 }}>
+            {product.name}
+        </Text>
+    )
+}
