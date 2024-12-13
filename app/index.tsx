be23800564222e034be890322403a310defe7e53
amang9446 <dev.aman7571@gmail.com>
2024-12-13 23:34:21 +0530

    feat: added various UI elements from gluestack-ui and rendered products on the home screen

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,13 @@
-import { FlatList, Text } from "react-native";
+import { FlatList, Text, View } from "react-native";
 import products from '../assets/products.json';
 import ProductListItem from "../components/ProductListItem";
 export default function HomeScreen() {
     return (
         <FlatList
             data={products}
+            numColumns={2}
+            contentContainerClassName="gap-2"
+            columnWrapperClassName="gap-2"
             renderItem={({ item }) => (
                 <ProductListItem product={item} />
             )}
