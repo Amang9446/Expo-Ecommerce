b7981ad8b57d1176a10757dc0c18fb13951e51c2
amang9446 <dev.aman7571@gmail.com>
2024-12-14 12:36:25 +0530

    fix: fixed responsiveness for web and mobile

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,15 @@
-import { FlatList, Text, View } from "react-native";
+import { FlatList, Text, useWindowDimensions, View } from "react-native";
 import products from '../assets/products.json';
 import ProductListItem from "../components/ProductListItem";
 export default function HomeScreen() {
+    const { width } = useWindowDimensions();
+    const columnNumber = width > 700 ? 3 : 2
     return (
         <FlatList
             data={products}
-            numColumns={2}
-            contentContainerClassName="gap-2"
+            key={columnNumber}
+            numColumns={columnNumber}
+            contentContainerClassName="gap-2 max-w-[960px] mx-auto w-full"
             columnWrapperClassName="gap-2"
             renderItem={({ item }) => (
                 <ProductListItem product={item} />
